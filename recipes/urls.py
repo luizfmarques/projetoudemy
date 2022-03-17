3ce56690329134f26965320f8efc766f11cecdc5
Luiz Fernando Marques <luiz_icb@hotmail.com>
2022-03-17 11:16:56 -0300

    atualizado url

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from recipes.views import home
+from recipes import views
 
 
 urlpatterns = [
-    path('', home),  # Home
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Fernando'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Luiz Fernando'
+    })
