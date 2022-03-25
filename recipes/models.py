529fd6cbc4c7afe27ea052f2e33613bf85ffcb00
Luiz Fernando Marques <luiz_icb@hotmail.com>
2022-03-25 13:59:08 -0300

    adicionando info reais

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     tittle = models.CharField(max_length=50)
@@ -26,3 +29,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.tittle
+    
